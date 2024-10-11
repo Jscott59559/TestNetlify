cf81c1c04efc2ef6770d8a65d878999d07b3b898
John8oy3456 <jscott59559@gmail.com>
2024-10-11 16:37:33 +0100

    redirect

--- src/api.js
+++ src/api.js
@@ -6,9 +6,7 @@ const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({
-        'hello': 'world'
-    })
+    res.json({"Hello": "World"})
 })
 
 app.use('/.netlify/functions/api', router)
